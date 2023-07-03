29a5185c6add797a7c54d96acabb4dd07e53520d
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-03 12:05:16 -0300

    arreglamos ubicacion y metimos postman

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.get('/dea', async (req, res) =>
 {
-    const dea = await DEAService.getMasCerc(req.header("UserLocation"));
+    const dea = await DEAService.getMasCerc(req.body);
     res.status(200).send(dea);
 });
 
