2c8ca699457a45099a04dab339f20ece008fa191
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-12 09:52:11 -0300

    cambiamos el puerto del back para subirlo a fl0

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import DEAService from "./src/services/dea-services.js";
 import UserService from "./src/services/user-services.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(cors());
 
